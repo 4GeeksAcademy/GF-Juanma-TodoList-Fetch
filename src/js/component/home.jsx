7d3c9f141a8dcf7444bfd88dc556aead33be39a7
JuanmaGF <137336081+JuanmaGF@users.noreply.github.com>
2024-01-17 17:49:15 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
 	const [inputValue, setInputValue] = useState("");
@@ -11,15 +11,71 @@ const Home = () => {
 
 	function handleKeyPress(e) {
 		if (e.key == "Enter") {
-			setLista(prev => prev.concat([inputValue]))
-			setInputValue("")
-		}
-
+				let body = lista.concat([{"label": inputValue, "done": false}])
+				fetch('https://playground.4geeks.com/apis/fake/todos/user/juanmagf', {
+					method: 'PUT',
+					body: JSON.stringify(body),
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				})
+					.then(response => {
+						if (!response.ok) throw Error(response.statusText);
+						return response.json();
+					})
+					.then(data => {
+						console.log(data);
+						setLista(prev => prev.concat([{"label": inputValue, "done": false}]))
+						setInputValue("")
+					}
+					)
+					.catch(error => console.error(error));
+			
+			
+		};
 	}
 
+	useEffect(() => {
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/juanmagf', {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(response => {
+				if (!response.ok) throw Error(response.statusText);
+				return response.json();
+			})
+			.then(data => {
+				setLista(data);
+			}
+			)
+			.catch(error => console.error(error));
+	}, []);
+
+
+
 	function eliminarTarea(event) {
-		setLista(prev => prev.filter((x,i) => i !== event));
-		}
+		
+		let body = lista.filter((x, i) => i !== event);
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/juanmagf', {
+					method: 'PUT',
+					body: JSON.stringify(body),
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				})
+					.then(response => {
+						if (!response.ok) throw Error(response.statusText);
+						return response.json();
+					})
+					.then(data => {
+						console.log(data);
+						setLista(prev => prev.filter((x, i) => i !== event));
+						setInputValue("")
+					}
+					)
+					.catch(error => console.error(error));
+	}
 
 	return (
 		<div className="text-center">
@@ -34,10 +90,11 @@ const Home = () => {
 				placeholder="What needs to be done"
 				style={{ width: '300px', backgroundColor: 'white', border: 'none' }}
 			/>
-			<ul className='list-group' style={{marginTop: '2px'}}>
+			<ul className='list-group' style={{ marginTop: '2px' }}>
 				{lista.map((nombre, i) => (
-					<li key={i} style={{ width: '300px', textAlign: 'left'}} className="list-group-item d-flex justify-content-start align-items-center mx-auto shadow-sm" >{nombre}
-						<span onClick={() => eliminarTarea(i)} style={{ cursor: 'pointer', marginLeft: 'auto'}}>
+					<li key={i} style={{ width: '300px', textAlign: 'left' }} className="list-group-item d-flex justify-content-start align-items-center mx-auto shadow-sm" >
+						{nombre.label}
+						<span onClick={() => eliminarTarea(i)} style={{ cursor: 'pointer', marginLeft: 'auto' }}>
 							<i className="fas fa-times "></i>
 						</span></li>
 				))}
